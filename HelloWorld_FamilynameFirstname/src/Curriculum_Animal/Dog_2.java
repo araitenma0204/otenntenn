6f8e3ebaf8cb282a05715d52a78d7e7671d050fb
user <user@172.16.20.18>
2025-07-03 22:12:48 +0900

    Curriculum_Animal_Dog_2.java

---
+++ HelloWorld_FamilynameFirstname/src/Curriculum_Animal/Dog_2.java
@@ -0,0 +1,21 @@
+package Curriculum_Animal;
+
+public class Dog_2 {
+	
+	//動物の名前の変数をフィールドに定義
+	String name;
+
+	//動物の数の変数をフィールドに定義
+	int num;
+
+	//nameに「犬」を代入するコンストラクタ
+	public Dog_2() {
+		name = "犬";
+	}
+	
+	//numに引数を代入するコンストラクタ
+	public Dog_2(int num) {
+		this.num = num;
+
+	}
+}
