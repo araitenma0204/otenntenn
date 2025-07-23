06eb7013c897d02d3beb203597b87ad194cbb0eb
user <user@172.16.20.14>
2025-07-23 22:37:01 +0900

    セッターゲッター削除修正済み

--- Sushi_app_araitenma/src/curriculum_profile3/Person.java
+++ Sushi_app_araitenma/src/curriculum_profile3/Person.java
@@ -49,45 +49,4 @@ public class Person {
 		System.out.println("合計" + Person.count + "人です");
 	}
 
-	// ここからgetterとsetter追加
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public int getAge() {
-		return age;
-	}
-
-	public void setAge(int age) {
-		this.age = age;
-	}
-
-	public double getHeight() {
-		return height;
-	}
-
-	public void setHeight(double height) {
-		this.height = height;
-	}
-
-	public double getWeight() {
-		return weight;
-	}
-
-	public void setWeight(double weight) {
-		this.weight = weight;
-	}
 }
